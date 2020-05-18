efad34591d4cc2803c3f62a941e50553830a82bc
Connor Larson <connorlarson@Connors-MacBook-Pro.local>
2020-05-18 09:55:16 -0500

    test

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,6 @@ import "@fortawesome/fontawesome-free/css/fontawesome.min.css";
 import "@fortawesome/fontawesome-free/css/fontawesome.css";
 
 class Header extends Component {
-
    
    render() {
       const social=[
